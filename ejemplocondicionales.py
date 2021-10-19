563a45603393af9fc8124cc6486e89d643961b48
martamp92 <martamaciasporras@gmail.com>
2021-10-19 18:20:59 +0200

    commit

---
+++ ejemplocondicionales.py
@@ -0,0 +1,15 @@
+#If (condición) : 
+    #Intruscciones del IF
+    #Dentro del IF
+#Instrucciones fuera del IF
+
+#Operadores de comparación, son los siguientes: 
+#< Menor, >Mayor, <=Menor o igual, >= Mayor o igual, ==igual, !=distinto.
+
+print("Ejemplo de condicionales: ")
+edad = 18
+if (edad >= 18):
+    print ("Mayor de edad")
+
+
+print("Fin de programa")
---
+++ estaciones.py
@@ -0,0 +1,15 @@
+print("ejemplo de condicionales")
+print("Introduzca una estación (1-4)")
+estacion = int(input())
+if (estacion == 1):
+    print("VERANO")
+elif (estacion == 2):
+    print("INVIERNO")
+elif (estacion == 3):
+    print("PRIMAVERA")
+elif (estacion == 4):
+    print("OTOÑO")
+else :
+    print("OPCIÓN INCORRECTA")
+    
+print("Fin de programa")
