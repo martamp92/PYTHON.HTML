76515d85a3a9f56c3f9fce4f1a1ca4082ca6f858
martamp92 <martamaciasporras@gmail.com>
2021-10-21 17:24:58 +0200

    commit

--- conjeturacollatz.py
+++ conjeturacollatz.py
@@ -1,2 +1,10 @@
 print("Introduca su número: ")
 numero = int(input())
+#Mientras que l número sea distinto de 1...
+while (numero != 1):
+    if (numero % 2 ==0):
+        numero = int(numero / 2)
+    else:
+        numero = numero * 3 +1
+    print(numero)
+print("Fin de programa")
