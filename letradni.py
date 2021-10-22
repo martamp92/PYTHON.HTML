692d5cd4a2ecb09aeae5279a8ba03abcb4869097
martamp92 <martamaciasporras@gmail.com>
2021-10-22 19:54:33 +0200

    commit

---
+++ letradni.py
@@ -0,0 +1,12 @@
+from math import trunc
+#DEBEMOS USAR LA LIBRERIA MATH PARA USAR ENTERO (TRUNCAR)
+print("Programa para validar DNI")
+print("Introduzca su número de dni")
+aux = input()
+if (aux.isdigit() == False):
+    print("Debe introducir NUMEROS SOLAMENTE")
+else:
+    numerodni = int(aux)
+    resultado = (numerodni - (trunc(numerodni / 23) * 23))
+    print(resultado)
+ 
