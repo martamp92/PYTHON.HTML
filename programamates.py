15a6d7f371b92ea58ec492b78e57acbe6022c24b
martamp92 <martamaciasporras@gmail.com>
2021-10-26 16:21:45 +0200

    commit

--- programamates.py
+++ programamates.py
@@ -24,21 +24,32 @@ print("Introduzca número1")
 numero1 = int(input())
 print("Introduzca número2")
 numero2 = int(input())
-mostrarMenu()
-opcion = int(input())
-if opcion == 1:
-    resultado = sumarNumero(numero1, numero2)
-    print("La suma es: " +str(resultado))
-elif opcion ==2:
-    resultado = restarNumeros(numero1, numero2)
-    print("La resta es: " + str(resultado))
-elif opcion == 3:
-    resultado = multiplicarNumeros(numero1, numero2)
-    print("El resultado es: " + str(resultado))
-elif opcion == 4:
-    resultado = dividirNumeros(numero1, numero2)
-    print("El restulado es: " + str(resultado))
-else:
-    print("Fin de programa")
-
-
+opcion = -1
+while (opcion != 0):
+    mostrarMenu()
+    opcion = int(input())
+    if opcion == 1:
+        resultado = sumarNumero(numero1, numero2)
+        print("La suma es: " +str(resultado))
+    elif opcion ==2:
+        resultado = restarNumeros(numero1, numero2)
+        print("La resta es: " + str(resultado))
+    elif opcion == 3:
+        resultado = multiplicarNumeros(numero1, numero2)
+        print("El resultado es: " + str(resultado))
+    elif opcion == 4:
+        resultado = dividirNumeros(numero1, numero2)
+        print("El restulado es: " + str(resultado))
+    elif opcion == 0:
+        print("Hasta luego")
+        break
+    else:
+        print("Opción incorrecta")
+    print("¿Desea introducir otro números? (s/n)")
+    respuesta = input()
+    if (respuesta == "s"):
+        print("Introduzca número1")
+        numero1 = int(input())
+        print("Introduzca número2")
+        numero2 = int(input())
+print("Fin de programa")
